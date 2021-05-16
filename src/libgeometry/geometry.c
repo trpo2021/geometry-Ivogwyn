647b38999520ed5d7bd4124bcf60d00dcf294337
Vlad Gutarov <Ivogwyn@users.noreply.github.com>
2021-05-16 17:33:11 +0700

    Add exit status

--- src/libgeometry/geometry.c
+++ src/libgeometry/geometry.c
@@ -5,13 +5,11 @@ void parcer(char* str, struct circle *c)
     char proof_str[] = "circle";
     int i = 0;
     int lenght_circle = strlen(proof_str);
-
-    //float x, y, r;
     printf("\n");
 
     if (strncmp(proof_str, str, lenght_circle) != 0) {
-        printf("Error,circle is written incorrectly\n");
-        exit(0);
+        printf("Error 1,circle is written incorrectly\n");
+        exit(1);
     }
     i = i + lenght_circle;
 
@@ -19,8 +17,8 @@ void parcer(char* str, struct circle *c)
         i++;
 
     if (str[i] != '(') {
-        printf("Error, '(' not found after circle) \n");
-        exit(0);
+        printf("Error 2, '(' not found after circle) \n");
+        exit(2);
     }
     i++;
 
@@ -28,15 +26,15 @@ void parcer(char* str, struct circle *c)
         i++;
 
     if (isdigit(str[i]) == 0) {
-        printf("Error, digit was expected\n");
-        exit(0);
+        printf("Error 3, digit was expected after '('\n");
+        exit(3);
 
     } else {
         char* xproof = &str[i];
         c->x = strtof(xproof, &xproof);
         if (*xproof != ' ') {
-            printf("Error, space was expected after '('\n");
-            exit(0);
+            printf("Error 4, space was expected after '('\n");
+            exit(4);
         }
     }
     while (str[i] != ' ')
@@ -46,15 +44,15 @@ void parcer(char* str, struct circle *c)
         i++;
 
     if (isdigit(str[i]) == 0) {
-        printf("Error, digit was expected after x\n");
-        exit(0);
+        printf("Error 5, digit was expected after x\n");
+        exit(5);
 
     } else {
         char* yproof = &str[i];
         c->y = strtof(yproof, &yproof);
         if (*yproof != ' ' && *yproof != ',') {
-            printf("Error, space or comma was expected after y\n");
-            exit(0);
+            printf("Error, 6 space or comma was expected after y\n");
+            exit(6);
         }
     }
     while ((str[i] != ' ') && (str[i] != ','))
@@ -66,8 +64,8 @@ void parcer(char* str, struct circle *c)
     if (str[i] == ',')
         i++;
     else {
-        printf("Error, comma was expected after y\n");
-        exit(0);
+        printf("Error 7, comma was expected after y\n");
+        exit(7);
     }
 
     while (str[i] == ' ') {
@@ -75,15 +73,15 @@ void parcer(char* str, struct circle *c)
     }
 
     if (isdigit(str[i]) == 0) {
-        printf("Error, digit was expected after comma\n");
-        exit(0);
+        printf("Error 8, digit was expected after comma\n");
+        exit(8);
 
     } else {
         char* rproof = &str[i];
         c->r = strtof(rproof, &rproof);
         if (*rproof != ' ' && *rproof != ')') {
-            printf("Error, space or ')' was expected after r\n");
-            exit(0);
+            printf("Error 9, space or ')' was expected after r\n");
+            exit(9);
         }
     }
 
@@ -97,8 +95,8 @@ void parcer(char* str, struct circle *c)
     if (str[i] == ')')
         i++;
     else {
-        printf("Error, ')' was expected after r\n");
-        exit(0);
+        printf("Error 10, ')' was expected after r\n");
+        exit(10);
     }
 
     while (str[i] == ' ')
